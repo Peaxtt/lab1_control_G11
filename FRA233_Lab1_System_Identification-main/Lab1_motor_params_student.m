fdc5db0b761d0cb5c89f65fb4769a2e890da0c0b
Kortor2548 <Sukrittathamchaisak@gmail.com>
2026-02-04 22:04:28 +0700

    .
    .

--- FRA233_Lab1_System_Identification-main/Lab1_motor_params_student.m
+++ FRA233_Lab1_System_Identification-main/Lab1_motor_params_student.m
@@ -6,20 +6,23 @@ This script for prepare data and parameters for parameter estimator.
 %}
 
 % R and L from experiment
-motor_R = 1.00;
-motor_L = 0.01;
+motor_R = 3.34;
+motor_L = 0.04;
 % Optimization's parameters
 motor_Eff = 0.5;
 motor_Ke = 0.05;
 motor_J = 0.001;
 motor_B = 0.0001;
 
-% Extract collected data
-Input = data{1}.Values.Data;
+% Extrcact collected data
+Input = data{2}.Values.Data;
 Time = data{1}.Values.Time;
-Velo = double(data{2}.Values.Data);
+Velo = double(data{1}.Values.Data);
 
 % Plot 
-figure(Name='Motor velocity response')
-plot(Time,Velo,Time,Input)
+Input = squeeze(double(data{2}.Values.Data));
+Time  = double(data{1}.Values.Time);
+Velo  = squeeze(double(data{1}.Values.Data));
+
+plot(Time, Velo, Time, Input)
 
